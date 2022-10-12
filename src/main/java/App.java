e1c764c96c0a9bbb0bf8390775f8ad2364ce5566
Viktor <viktorkyndrat@gmail.com>
2022-10-12 22:12:59 +0300

    Gradll

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,12 @@
+import com.google.gson.GsonBuilder;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hellov");
-    }
+
+            User user = new User();
+            user.setName("Victor");
+            user.setLastName("Kundrat");
+            String json = new GsonBuilder().create().toJson(user);
+            System.out.println(json);
+}
 }
---
+++ src/main/java/User.java
@@ -0,0 +1,22 @@
+public class User {
+
+        public String name;
+        public String lastName;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+    }
+
